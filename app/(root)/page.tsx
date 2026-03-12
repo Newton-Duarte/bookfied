9a98a284299bea198468455fca14728214838c01
Newton Duarte <newton_@hotmail.com>
2026-03-12 16:19:06 -0300

    feat: fetch home page books from database

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,16 @@
 import BookCard from "@/components/book-card";
 import HeroSection from "@/components/hero-section";
-import { sampleBooks } from "@/lib/constants";
+import { getAllBooks } from "@/lib/actions/book.actions";
 
 const Page = async () => {
+  const booksResult = await getAllBooks()
+  const books = booksResult.success ? booksResult.data ?? [] : []
+
   return (
     <main className="wrapper container">
       <HeroSection />
       <div className="library-books-grid">
-        {sampleBooks.map((book) => (
+        {books.map((book) => (
           <BookCard key={book._id} title={book.title} author={book.author} coverURL={book.coverURL} slug={book.slug} />
         ))}
       </div>
--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'covers.openlibrary.org' }
+      { protocol: 'https', hostname: 'covers.openlibrary.org' },
+      { protocol: 'https', hostname: '4alnz2r52wceuufh.public.blob.vercel-storage.com' }
     ]
   }
 };
